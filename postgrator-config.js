d8c881b9a212fa1b72c4c395945962ede7b5c63b
Sean Johnson <seanjohnson220@gmail.com>
2021-02-24 15:16:38 -0800

    update to postgrator file

--- postgrator-config.js
+++ postgrator-config.js
@@ -3,6 +3,10 @@ require("dotenv").config();
 module.exports = {
 	migrationsDirectory: "migrations",
 	driver: "pg",
-	connectionString: process.env.DATABASE_URL || "postgresql://addresshero_boss@localhost/addresshero_database"
-	// connectionString: process.env.DATABASE_URL_TEST,
+	connectionString: process.env.DATABASE_URL || "postgresql://addresshero_boss@localhost/addresshero_database",
+	"host": process.env.MIGRATION_DB_HOST,
+	"port": process.env.MIGRATION_DB_PORT,
+	"database": process.env.MIGRATION_DB_NAME,
+	"username": process.env.MIGRATION_DB_USER,
+	"password": process.env.MIGRATION_DB_PASS
 };
