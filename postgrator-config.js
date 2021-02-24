0290cd20d423f675ca325ca96b559b0469eb58cf
Sean Johnson <seanjohnson220@gmail.com>
2021-02-24 14:37:48 -0800

    update to postgrator file

--- postgrator-config.js
+++ postgrator-config.js
@@ -3,6 +3,6 @@ require("dotenv").config();
 module.exports = {
 	migrationsDirectory: "migrations",
 	driver: "pg",
-	// connectionString: process.env.DATABASE_URL,
-	connectionString: process.env.DATABASE_URL_TEST,
+	connectionString: process.env.DATABASE_URL || "postgresql://addresshero_boss@localhost/addresshero_database"
+	// connectionString: process.env.DATABASE_URL_TEST,
 };
