e902b4da8b68e415e4e7ee0b3c2dc74edba5f6e0
Sean Johnson <seanjohnson220@gmail.com>
2021-02-24 14:21:03 -0800

    update to timeline test

--- test/timeline-endpoints.spec.js
+++ test/timeline-endpoints.spec.js
@@ -2,7 +2,7 @@ const knex = require("knex");
 const app = require("../src/app");
 const helpers = require("./test-helpers");
 
-describe.only("Timeline Endpoints", function () {
+describe("Timeline Endpoints", function () {
 	let db;
 
 	const {
