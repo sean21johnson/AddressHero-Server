f557fe1ba6d0fd9ef64bd6715897ed1a59224138
Sean Johnson <seanjohnson220@gmail.com>
2021-02-25 06:31:42 -0800

    update to postgrator file

--- postgrator-config.js
+++ postgrator-config.js
@@ -11,5 +11,5 @@ module.exports = {
 	"database": process.env.MIGRATION_DB_NAME,
 	"username": process.env.MIGRATION_DB_USER,
 	"password": process.env.MIGRATION_DB_PASS,
-	// ssl: true
+	ssl: true
 };
